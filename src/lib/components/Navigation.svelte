41d856b8367cf9c20e8404ce2c5982a02cbd9e57
Giuseppe De Palma <dpl.gsp@tutanota.com>
2024-04-11 10:36:07 +0100

    add sidebar

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,48 @@
+<script>
+    import {
+        TreeView,
+        TreeViewItem,
+        getDrawerStore,
+    } from "@skeletonlabs/skeleton";
+
+    export let services;
+
+    const drawerStore = getDrawerStore();
+
+    function drawerClose() {
+        drawerStore.close();
+    }
+
+    export let sendMessage;
+
+    function sendMsg(name) {
+        return () => {
+            sendMessage(name);
+            drawerClose();
+        };
+    }
+</script>
+
+<div
+    class="flex h-full flex-col items-center rounded-tr-lg bg-surface-500 text-white shadow"
+>
+    <div aria-current="page" aria-label="Homepage" class="mt-4">
+        <span class="p-3 font-arista text-4xl h-16 w-56 underline">
+            MastroGPT
+        </span>
+    </div>
+    <nav class="w-full p-4">
+        <hr class="mb-8" />
+        {#each Object.keys(services) as name}
+            <TreeView class="rounded-r-lg">
+                <button
+                    {name}
+                    on:click={sendMsg(name)}
+                    class="w-full text-center text-lg"
+                >
+                    <TreeViewItem>{name}</TreeViewItem>
+                </button>
+            </TreeView>
+        {/each}
+    </nav>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,29 @@
-<link rel="stylesheet" href="/css/almond.min.css" />
 <script>
+    import "../app.css";
+
     import { onMount } from "svelte";
-    let base = document.location.href.replace(/index\.html$/, "");
+    import Navigation from "../lib/components/Navigation.svelte";
+    import { AppBar, AppShell, getDrawerStore } from "@skeletonlabs/skeleton";
+
+    // Components & Utilities
+    import { Modal, Drawer } from "@skeletonlabs/skeleton";
+    import { page } from "$app/stores";
+    import Chat from "$lib/components/Chat.svelte";
+
+    const drawerStore = getDrawerStore();
+
+    function drawerOpen() {
+        drawerStore.open({});
+    }
+
     let services = {};
 
     onMount(() => {
-        fetch(base + "api/my/mastrogpt/index")
+        fetch($page.url.href + "api/my/mastrogpt/index")
             .then((x) => x.json())
             .then((data) => {
                 for (const service of data["services"]) {
-                    let name = ""+service.name
+                    let name = "" + service.name;
                     services[name] = service.url;
                 }
             })
@@ -19,23 +33,79 @@
             });
     });
 
-    function postMessage(event) {
-        let name = event.target.name
-        let url =  base + "api/my/" + services[name]
-        let chat = document.getElementById("chat").contentWindow
-        chat.postMessage({ type: "chat", name: name, url: url });
-    }
+    let receiveMessage;
+
+    let sendMessage = (name) => {
+        let url = $page.url.href + "api/my/" + services[name];
+        receiveMessage(name, url);
+    };
 </script>
 
-<link rel="stylesheet" href="/css/index.css" />
+<!-- Overlays -->
+<Drawer><Navigation {services} {sendMessage} /></Drawer>
+<Modal />
 
-<div id="top-area">
-    <h2>MastroGPT</h2>
-    {#each Object.keys(services) as name}
-        <button  name={name} on:click={postMessage}>{name}</button>
-    {/each}
+<!-- (AppShell) -->
+<div class="flex h-full w-auto flex-col justify-between">
+    <AppShell
+        slotSidebarLeft="bg-surface-500/5 w-0 md:w-64"
+        slotPageHeader="md:hidden"
+        regionPage={"scroll-smooth"}
+    >
+        <!-- (header) -->
+        <svelte:fragment slot="sidebarLeft">
+            <Navigation {services} {sendMessage} />
+        </svelte:fragment>
+        <!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+        <svelte:fragment slot="pageHeader"
+            ><AppBar>
+                <svelte:fragment slot="trail">
+                    <div
+                        class="flex flex-row-reverse items-center justify-between w-full"
+                    >
+                        <button
+                            class="btn btn-sm md:mr-4"
+                            on:click={drawerOpen}
+                        >
+                            <span>
+                                <svg
+                                    viewBox="0 0 100 80"
+                                    class="fill-token h-4 w-4"
+                                >
+                                    <rect width="100" height="20" />
+                                    <rect y="30" width="100" height="20" />
+                                    <rect y="60" width="100" height="20" />
+                                </svg>
+                            </span>
+                        </button>
+
+                        <span
+                            class="font-arista text-2xl font-bold text-surface-600 ml-2"
+                        >
+                            MastroGPT
+                        </span>
+                    </div>
+                </svelte:fragment>
+
+                <!-- <svelte:fragment slot="trail"><LightSwitch /></svelte:fragment> -->
+            </AppBar></svelte:fragment
+        >
+
+        <!-- Content -->
+        <div class="relative h-full w-full p-2 py-4">
+            <Chat bind:receiveMessage />
+        </div>
+        <!-- <iframe title="chat" id="chat" src="/chat"></iframe>
+                 -->
+    </AppShell>
 </div>
-<div id="iframe-container">
+
+<!-- 
+<link rel="stylesheet" href="/css/almond.min.css" />
+
+<link rel="stylesheet" href="/css/index.css" /> -->
+
+<!-- <div id="iframe-container">
     <iframe title="chat" id="chat" src="/chat"></iframe>
     <iframe title="display" id="display" src="/display"></iframe>
-</div>
+</div> -->
